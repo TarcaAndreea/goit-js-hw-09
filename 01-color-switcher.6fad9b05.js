a8bc7c3c56f4db5562d4e854450c3b9d62b11b27
TarcaAndreea <123555392+TarcaAndreea@users.noreply.github.com>
2023-06-24 13:08:51 +0000

    Deploying to gh-pages from @ TarcaAndreea/goit-js-hw-09@74fd641a01646ef0a6ee6f0376ddf9136d054963 🚀

---
+++ 01-color-switcher.6fad9b05.js
@@ -0,0 +1,2 @@
+"use strict";const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");let o=null;t.addEventListener("click",function(){o=setInterval(()=>{let t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,"0")}`;n.style.backgroundColor=t},1e3),t.disabled=!0}),e.addEventListener("click",function(){clearInterval(o),t.disabled=!1});
+//# sourceMappingURL=01-color-switcher.6fad9b05.js.map
---
+++ 01-color-switcher.d5fa0922.js
@@ -0,0 +1,2 @@
+!function(){"use strict";var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),o=null;t.addEventListener("click",function(){o=setInterval(function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,"0"));n.style.backgroundColor=t},1e3),t.disabled=!0}),e.addEventListener("click",function(){clearInterval(o),t.disabled=!1})}();
+//# sourceMappingURL=01-color-switcher.d5fa0922.js.map
